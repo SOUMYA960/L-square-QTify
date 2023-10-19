09b047dcc44d09bb9e23ffbd431e51e3fdd3e9aa
SOUMYA960 <soumyadeep9434@gmail.com>
2023-10-19 12:31:35 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+// import Card from "./components/Card/Card";
+import { useEffect, useState } from "react";
+import { fetchTopAlbums, fetchNewAlbums } from "./api";
+import Section from "./Components/Section";
 
 function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateTopAlbumbsData = async () => {
+    const data = await fetchTopAlbums();
+    setTopAlbumsData(data);
+  };
+
+  useEffect(() => {
+    generateTopAlbumbsData();
+  }, []);
+
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
+
+  const generateNewAlbumbsData = async () => {
+    const data = await fetchNewAlbums();
+    setNewAlbumsData(data);
+  };
+
+  useEffect(() => {
+    generateNewAlbumbsData();
+  }, []);
   return (
     <div>
       <Navbar />
+      <Hero />
+      {/* {topAlbumsData.map((item) => {
+        return <Card data={item} type="album" key={item.title} />;
+      })} */}
+      <div>
+        <Section data={topAlbumsData} type="album" title="Top Albums" />
+        <Section data={newAlbumsData} type="album" title="New Albums" />
+        {/* <Section data={topAlbumsData} type="album" title="Songs" /> */}
+      </div>
     </div>
   );
 }
